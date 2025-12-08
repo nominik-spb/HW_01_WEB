f92283e54c9c45204c6db81277fce8936fea6a0b
nominik-spb <nominik.spb@gmail.com>
2025-12-08 20:42:11 +0300

    change_081225

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
 
-package ru.netology;
-
-import java.io.*;
-import java.net.ServerSocket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) throws IOException {
         Server server = new Server();
+        server.run();
+        //server.processingConnection();
     }
 }
